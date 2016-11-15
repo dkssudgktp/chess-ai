81893943fdbf6d191973b8c86cc93cfa032ed7ea
Hi:D <hellomydir@gmail.com>
2016-11-15 15:21:33 +0900

    개발 합시다

---
+++ src/chess/Board.java
@@ -0,0 +1,30 @@
+package chess;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Board extends JFrame implements MouseListener{
+	
+	JPanel [][] squares; 
+	public static void main(String[] args) {
+		
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+}
