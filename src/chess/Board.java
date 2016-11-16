a4cf965e50cd561186287afcea4fd9b17cdb9061
Hi:D <hellomydir@gmail.com>
2016-11-16 20:33:59 +0900

    selection add

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -5,7 +5,9 @@ import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.StringTokenizer;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -17,17 +19,17 @@ public class Board extends JFrame implements MouseListener{
 	 */
 	private static final long serialVersionUID = 1L;
 	JPanel [][] squares;
-	
+
 	public void addPiece(Piece p,int i, int j){
         squares[i][j].add(p);
         paintAll(getGraphics());
     }
-	
+
 	public void removePiece(int x, int y){
         squares[x][y].remove(0);
         paintAll(getGraphics());
     }
-	
+
 	public Board(){
 		Container c = getContentPane();
 		c.setLayout(new GridLayout(8, 8));
@@ -35,26 +37,40 @@ public class Board extends JFrame implements MouseListener{
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
 				squares[i][j] = new JPanel();
-				if ((i+j)%2 == 0) 
+				squares[i][j].setName(String.valueOf(i)+'.'+String.valueOf(j));
+				if ((i+j)%2 == 0)
 					squares[i][j].setBackground(Color.decode("#FFC489"));
-				else 
+				else
 					squares[i][j].setBackground(Color.decode("#C77931"));
 				squares[i][j].addMouseListener(this);
 				c.add(squares[i][j]);
 			}
 		}
-		
+
 		ImageIcon img = new ImageIcon("img/icon.png");
 		this.setIconImage(img.getImage());
-		this.setSize(700,700);
-		this.setTitle("beat me if you can");
+		this.setSize(550,550);
+		this.setTitle("Chess");
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
 	}
+	
+	private void pickup(int x, int y) {
+		squares[x][y].setBorder(BorderFactory.createLineBorder(Color.red,4));
+	}
+	
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		System.out.println(e.getSource());
+		JPanel j = (JPanel)e.getSource();
+		StringTokenizer position = new StringTokenizer(j.getName(), ".");
+		
+		if (j.getBorder() != null) {
+			j.setBorder(null);
+		}
+		else {
+			pickup(Integer.parseInt(position.nextToken()), Integer.parseInt(position.nextToken()));
+		}
 	}
 	@Override
 	public void mousePressed(MouseEvent e) {
--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -1,20 +1,24 @@
 package chess;
 
+
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class Piece extends JLabel{
 	/**
-	 * 말의 이미지를 띄우기 위해 jlabel 생성 하는 곳 
+	 * 말의 이미지를 띄우기 위해 jlabel 생성 하는 곳
 	 */
 	private static final long serialVersionUID = 1L;
 	public Piece() {
-		super(new ImageIcon("Black_King.png"));
+		super(new ImageIcon("Black_King.png"), JLabel.CENTER);
 	}
-	
+
 	public Piece(String image_file)
     {
-        super(new ImageIcon(image_file));
+        super(new ImageIcon(image_file), JLabel.CENTER);
     }
 	public static void main(String[] args) {
 		Board t = new Board();
