ed1a5755cf92d363ab60bae711c1737a47df0b99
Hi:D <hellomydir@gmail.com>
2016-11-18 11:49:28 +0900

    chess pieces add

---
+++ src/chess/Game.java
@@ -0,0 +1,22 @@
+package chess;
+
+public class Game {
+	public Game() {
+		// TODO Auto-generated constructor stub
+	}
+	String[][] table = new String[8][8];
+	String[] pieces = {"Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook" };
+	
+	private void newGame() {
+		for (int i = 0; i < pieces.length; i++) {
+			
+		}
+		
+		for (int i = 0; i < 8; i++) {
+			table[1][i] = "Black_Pawn";
+		}
+		for (int i = 0; i < 8; i++) {
+			table[6][i] = "White_Pawn";
+		}
+	}
+}
