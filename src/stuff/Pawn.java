e0004df83791a0f6c74d3a1cc1608aec0ab1494c
revsic <revsic99@gmail.com>
2016-11-21 21:16:16 +0900

    Fix Pawn can move two blocks when first move

--- src/stuff/Pawn.java
+++ src/stuff/Pawn.java
@@ -2,13 +2,19 @@ package stuff;
 
 public class Pawn extends Pair {
   public Pawn() {
-    byte flag = (1 << 1);
+    byte flag = (1 << 1) | (1 << 2);
     byte[] dx = new byte[] { 0 };
     byte[] dy = new byte[] { 1 };
 
     byte[] adx = new byte[] { 1, -1 };
     byte[] ady = new byte[] { 1,  1 };
 
-    super("Pawn", flag, dx, dy, adx, ady);
+    byte[] tmp = new byte[] { 0, 2 };
+
+    super("Pawn", flag, dx, dy, adx, ady, tmp);
+  }
+
+  public void moved() {
+    flag = (1 << 1);
   }
 }
