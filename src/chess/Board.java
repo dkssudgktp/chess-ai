a152004523d70f3879bd4daee43d6949ab377912
revsic <revsic99@gmail.com>
2016-11-30 23:15:09 +0900

    Create dialog when game end

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -1,5 +1,7 @@
 package chess;
 
+import stuff.Pair;
+
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Frame;
@@ -15,6 +17,7 @@ import javax.swing.JPanel;
 import javax.swing.JOptionPane;
 
 public class Board extends JFrame implements MouseListener {
+	private boolean isGameEnd = false;
 	private boolean chosen;//선택 되었는지 확인 true 선택됨 false 선택 안됨
 	private int chosenx, choseny;
 	public static boolean isWhiteTurn = true;//누구의 차레인지 Ture:화이트, false:블랙
@@ -102,6 +105,14 @@ public class Board extends JFrame implements MouseListener {
 					squares[chosenx][choseny].setBorder(null);
 					chosen = false;
 
+					if (Game.isEnemy(gox, goy)) {
+						squares[gox][goy].removeAll();
+
+						if (Game.table[gox][goy].endsWith("King")) {
+							isGameEnd = true;
+						}
+					}
+
 					squares[gox][goy].add(squares[chosenx][choseny].getComponent(0));
 
 					Game.table[gox][goy] = Game.table[chosenx][choseny];
@@ -110,6 +121,17 @@ public class Board extends JFrame implements MouseListener {
 					isWhiteTurn = !isWhiteTurn;
 
 					paintAll(getGraphics());
+
+					if (isGameEnd) {
+						String winner = "White";
+						if (isWhiteTurn) {
+							winner = "Black";
+						}
+
+						JOptionPane.showMessageDialog(this, winner + " win !!");
+						System.exit(0);
+					}
+
 					break;
 				}
 			}
